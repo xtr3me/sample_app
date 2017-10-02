017caf00f69883adfad6cbd955fa3e5b4a1ffbbf
Peter Lohmann <hi@ploh.io>
2017-10-02 10:28:14 +0200

    A rank's scores must be ordered
    #score_from has to be lower than #score_to

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,4 +1,14 @@
 class Rank < ActiveRecord::Base
   validates :name, presence: true
 
+  validate :validate_scores_are_ordered_for_a_rank
+
+  private
+
+  def validate_scores_are_ordered_for_a_rank
+    unless score_from < score_to
+      errors.add(:score_from, 'must be lower than score_to')
+      errors.add(:score_to, 'must be higher than score_from')
+    end
+  end
 end
--- test/models/rank_test.rb
+++ test/models/rank_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class RankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "score_from should be lower than score_to" do
+    rank = ranks(:one)
+    rank.score_from = 20.0
+    rank.score_to = 19.0
+    assert_not rank.valid?, "Expected rank to be invalid"
+    assert_match /must be lower than/, rank.errors[:score_from].first
+    assert_match /must be higher than/, rank.errors[:score_to].first
+  end
 end
