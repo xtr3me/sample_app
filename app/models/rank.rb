32f1a6be992d8bd0e1a97c47ca81a130413e207a
Gustavo Cunha <gustavocunha@gmail.com>
2017-12-04 20:58:07 +0000

    Fix specs and add required features

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,4 +1,41 @@
 class Rank < ActiveRecord::Base
   validates :name, presence: true
 
+  validates :score_to, numericality: { greater_than: :score_from}
+  validates :score_to, format: /\A\d+\.*\d{0,1}\Z/
+  validates :score_from, format: /\A\d+\.*\d{0,1}\Z/
+
+  validate :score_ranges
+
+  private
+
+  def score_ranges
+    scores = Rank.order(:score_from).where.not(id: id).pluck(:score_from, :score_to)
+    scores.each do |score|
+      check_score_valid(score)
+    end
+  end
+
+  def check_score_valid(score)
+    errors.add(:score_from, 'overlaps with another range on system') if score_from_invalid?(score)
+    errors.add(:score_to, 'overlaps with another range on system') if score_to_invalid?(score)
+  end
+
+  def score_from_invalid?(score)
+    if score_from.between?(score[0], score[1])
+      return true
+    elsif score[0].between?(score_from, score_to)
+      return true
+    end
+    false
+  end
+
+  def score_to_invalid?(score)
+    if score_to.between?(score[0], score[1])
+      return true
+    elsif score[1].between?(score_from, score_to)
+      return true
+    end
+    false
+  end
 end
--- test/models/rank_test.rb
+++ test/models/rank_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class RankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'score_from should be lower than score_to' do
+    rank = Rank.new(name: 'Rank', score_from: 1.0, score_to: 0.9)
+    assert_not rank.valid?
+  end
+
+  test 'score_to should be higher than score_from' do
+    rank = Rank.new(name: 'Rank', score_from: 1.0, score_to: 0.9)
+    assert_not rank.valid?
+  end
+
+  test 'should not have overlaping other ranks - score_from within existing rank' do
+    rank = Rank.new(name: 'Rank', score_from: 1.4, score_to: 2.1)
+    assert_not rank.valid?
+  end
+
+  test 'should not have overlaping other ranks - score_to within existing rank' do
+    rank = Rank.new(name: 'Rank', score_from: 0.5, score_to: 1.4)
+    assert_not rank.valid?
+  end
+
+  test 'should not have overlaping other ranks - scores within existing range' do
+    rank = Rank.new(name: 'Rank', score_from: 1.1, score_to: 1.4)
+    assert_not rank.valid?
+  end
+
+  test 'should not have overlaping other ranks - existing range within scores' do
+    rank = Rank.new(name: 'Rank', score_from: 0.1, score_to: 1.6)
+    assert_not rank.valid?
+  end
+
+  test 'score_from should have a precision of 0.1' do
+    rank = Rank.new(name: 'Rank', score_from: 1.42, score_to: 2.1)
+    assert_not rank.valid?
+  end
+
+  test 'score_to should have a precision of 0.1' do
+    rank = Rank.new(name: 'Rank', score_from: 1.4, score_to: 2.21)
+    assert_not rank.valid?
+  end
 end
