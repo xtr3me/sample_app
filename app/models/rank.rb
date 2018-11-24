55b3db8fa9c7253e1bc812c308f579881fdc5e37
Melvin Voetberg <melvin.voetberg@ziggo.nl>
2018-11-24 15:43:08 +0100

    - added validation of `score_from` to be lower than `score_to` - added validation so scores cannot overlap with other Ranks - added validation so scores have a precision of one decimal places

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,4 +1,23 @@
 class Rank < ActiveRecord::Base
   validates :name, presence: true
+  validates_numericality_of :score_to, greater_than: :score_from
+  validate :check_range
+  validate :check_decimal
 
+	def check_range
+	    score_to_inrange = Rank.where("score_to >= ? AND score_from <= ?", score_to, score_to).first()
+	    score_from_inrange = Rank.where("score_to >= ? AND score_from <= ?", score_from, score_from).first()
+	    unless score_to_inrange.nil? && score_from_inrange.nil?
+	      errors.add(:base,"There is already a rank set for this score")
+	    end
+	end
+
+	def check_decimal
+		decimals = 1;
+		score_to_check = score_to * 10 * decimals;
+		score_from_check = score_from * 10 * decimals;
+		if (score_to_check % 1 != 0) || (score_from_check % 1 != 0)
+			errors.add(:base,"Scores need to have #{decimals} decimal places.")
+		end
+	end
 end
--- test/models/rank_test.rb
+++ test/models/rank_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class RankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		Rank.create(name: 'Sample', score_from: 10, score_to: 20)
+		@rank = Rank.new(name: 'Test', score_from: 50, score_to: 100)
+	end
+
+  test 'valid rank' do
+  	assert @rank.valid?
+  end
+
+  test 'rank without name' do
+		@rank.name = nil
+    refute @rank.valid?, 'saved rank without a name'
+    assert_not_nil @rank.errors[:name], 'no validation error for name present'
+  end
+
+  test 'rank with score_from > score_to' do
+  	@rank.score_to = 30
+    refute @rank.valid?, 'set score_to lower than score_from'
+    assert_not_nil @rank.errors[:name], 'no validation error for name present'
+  end
+
+  test 'rank with alphabetical scores' do
+  	@rank.score_to = "asdasdasd"
+    refute @rank.valid?, 'set score_to alphabetical'
+    assert_not_nil @rank.errors[:name], 'no validation error for name present'
+  end
+
+  test 'rank with score already in database' do
+  	@rank.score_from = 15
+    refute @rank.valid?, 'set score_from to score already in database'
+    assert_not_nil @rank.errors[:name], 'no validation error for name present'
+  end
+
+  test 'rank with more than one decimal' do
+  	@rank.score_to = 100.12345
+    refute @rank.valid?, 'set score_to with more than one decimal'
+    assert_not_nil @rank.errors[:name], 'no validation error for name present'
+  end
 end
