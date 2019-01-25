a41ca96b31a616e839d775e4ca3130237f5e8ea5
Onur Kucukkece <onurkucukkece@gmail.com>
2019-01-25 16:30:36 +0100

    added custom validation in order to check whether it's overlapping with other records

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,6 +1,8 @@
 class Rank < ActiveRecord::Base
   validates :name, :score_from, :score_to, presence: true
   validate :validate_scores
+  validate :not_overlapping
+
   # Returns instance's score_from and score_to as a range
   def range
     (score_from..score_to)
@@ -17,5 +19,13 @@ class Rank < ActiveRecord::Base
   # and return error if it's agains validtion rules
   def validate_scores
     errors.add(:score_from, 'has to be lower than score_to') if (score_from <=> score_to) > 0
+  end
+
+  # It will return error if it's overlappimg
+  def not_overlapping
+    Rank.ranges.each do |range|
+      errors.add(:score, 'should not overlap with other records') if range.include?(score_from) || range.include?(score_to)
+      return
+    end
   end
 end
