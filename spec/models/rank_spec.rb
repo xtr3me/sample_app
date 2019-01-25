41c7b58e133a214547529bef70be2becf50f371f
Onur Kucukkece <onurkucukkece@gmail.com>
2019-01-25 11:34:00 +0100

    added tests for rank model

--- spec/models/rank_spec.rb
+++ spec/models/rank_spec.rb
@@ -6,5 +6,15 @@ describe Rank, type: :model do
   it { should validate_presence_of(:score_from) }
   it { should validate_presence_of(:score_to) }
 
+  describe '#validate_scores' do
+    it 'should give an error if score_from is greather than score_to' do
+      rank = FactoryBot.create(:rank, score_from: 5, score_to: 2)
+      expect(rank).not_to be_valid
+    end
 
+    it 'should return error if score_to is lower than score_from' do
+      rank = FactoryBot.create(:rank, score_from: 5, score_to: 2)
+      expect(rank).not_to be_valid
+    end
+  end
 end
