019e4a45c158a8e9a1df892a200f6eb656d9b893
Onur Kucukkece <onurkucukkece@gmail.com>
2019-01-25 11:40:31 +0100

    added custom validation for scores compairing

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,4 +1,13 @@
 class Rank < ActiveRecord::Base
   validates :name, :score_from, :score_to, presence: true
+  validate :validate_scores
 
+  private
+
+  # Compares score_from and score_to
+  # and return error if it's agains validtion rules
+  def validate_scores
+    errors.add(:score_from, 'has to be lower than score_to') if (score_from <=> score_to) > 0
+    errors.add(:score_to, 'has to be higer than score_from') if (score_to <=> score_from)  < 0
+  end
 end
