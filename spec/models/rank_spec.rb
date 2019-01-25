6f107f4be289ce302ee6b2e4b4c2795e70eff70e
Onur Kucukkece <onurkucukkece@gmail.com>
2019-01-25 11:40:35 +0100

    fixed tests

--- spec/models/rank_spec.rb
+++ spec/models/rank_spec.rb
@@ -8,12 +8,12 @@ describe Rank, type: :model do
 
   describe '#validate_scores' do
     it 'should give an error if score_from is greather than score_to' do
-      rank = FactoryBot.create(:rank, score_from: 5, score_to: 2)
+      rank = FactoryBot.build(:rank, score_from: 5, score_to: 2)
       expect(rank).not_to be_valid
     end
 
     it 'should return error if score_to is lower than score_from' do
-      rank = FactoryBot.create(:rank, score_from: 5, score_to: 2)
+      rank = FactoryBot.build(:rank, score_from: 5, score_to: 2)
       expect(rank).not_to be_valid
     end
   end
